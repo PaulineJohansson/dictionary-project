b89673d87686d9ad5b806b3c2592c16ca300e503
PaulineJohansson <pauline89_johansson@hotmail.com>
2022-05-12 15:44:36 +0200

    Changed box-shadow

--- src/Phonetic.js
+++ src/Phonetic.js
@@ -8,7 +8,7 @@ export default function Phonetic(props) {
       <a href={props.phonetic.audio} target="_blank" rel="noreferrer">
         Listen
       </a>
-      <br />
+
       {props.phonetic.text}
     </div>
   );
